fb0bb358db5f66de688bcf6e7a361e6769631032
Andrew Sosa <andrew@andrewsosa.com>
2019-04-13 10:40:57 -0400

    Add Express model/routes

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 ### Outline
 * Setting up Express
+* Parsing body requests
+* What about logging?
+* Using Dotenv for env vars
 * Installing Mongoose
 * Creating a sample model
+* Writing routes for dotenv
 * What about nodemon?
-* Using Dotenv for env vars
 
 
 ## Snippets
@@ -29,4 +32,35 @@ db.once("open", function() {
 });
 ```
 
+### MongoDB Model
+```javascript
+const hackerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  school: { type: String, required: true }
+});
 
+const Hacker = new mongoose.model("Hacker", hackerSchema);
+
+```
+
+### MongoDB Routes
+```javascript
+app.get("/hackers", (req, res) => {
+  Hacker.find({}, (err, hackers) => {
+    res.send(hackers);
+  });
+});
+app.post("/hackers", (req, res) => {
+  const { name, email, school } = req.body;
+  const newHacker = Hacker({ name, email, school });
+  newHacker.save((err, obj) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send(err);
+    } else {
+      res.status(201).send(obj);
+    }
+  });
+});
+```
--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const { Hacker } = require("./models");
 
 const app = express();
 const port = 3000;
@@ -13,27 +16,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger("dev"));
 
-const hackers = [
-  {
-    name: "Andrew Sosa",
-    email: "team@makebuild.com",
-    school: "FSU"
-  },
-  {
-    name: "Ann Hacker",
-    email: "ann@hacker.org",
-    school: "FSU"
-  }
-];
+mongoose.connect("mongodb://localhost/workshop", { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => console.log("Connected to MongoDB!"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/hackers", (req, res) => res.send(hackers));
+app.get("/hackers", (req, res) => {
+  Hacker.find({}, (err, hackers) => {
+    res.send(hackers);
+  });
+});
 app.post("/hackers", (req, res) => {
   const { name, email, school } = req.body;
-  const newHacker = { name, email, school };
-  hackers.push(newHacker);
-  res.status(201).send(newHacker);
+  const newHacker = Hacker({ name, email, school });
+  newHacker.save((err, obj) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send(err);
+    } else {
+      res.status(201).send(obj);
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
