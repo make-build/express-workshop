8163457cd3a95500ae893af98cd3f50528e090f3
Andrew Sosa <andrew@andrewsosa.com>
2019-04-13 10:22:42 -0400

    Add body parser and morgan logger

--- README.md
+++ README.md
@@ -6,3 +6,27 @@
 * Creating a sample model
 * What about nodemon?
 * Using Dotenv for env vars
+
+
+## Snippets
+
+### Adding BodyParser
+```javascript
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+```
+
+
+### Connecting to Mongoose:
+```javascript
+mongoose.connect("mongodb://localhost/workshop", { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  // we're connected!
+  console.log("Connected to MongoDB!");
+});
+```
+
+
--- src/app.js
+++ src/app.js
@@ -3,9 +3,37 @@
  */
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger("dev"));
+
+const hackers = [
+  {
+    name: "Andrew Sosa",
+    email: "team@makebuild.com",
+    school: "FSU"
+  },
+  {
+    name: "Ann Hacker",
+    email: "ann@hacker.org",
+    school: "FSU"
+  }
+];
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/hackers", (req, res) => res.send(hackers));
+app.post("/hackers", (req, res) => {
+  const { name, email, school } = req.body;
+  const newHacker = { name, email, school };
+  hackers.push(newHacker);
+  res.status(201).send(newHacker);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
